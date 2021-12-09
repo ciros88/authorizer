3bf84882a2e13b22586f4b28f4e4b22f63fde505
Ciro Scognamiglio <c.scognamiglio@techgap.it>
2021-12-09 16:31:47 +0100

    META-INF dir added (with LICENSE and spring.factories)
    spring auto-configuration enabled through
    META-INF\spring.factories and @ComponentScan in
    AuthorizerConfig
    
    application.properties removed

---
+++ src/main/java/com/ciros/authorizer/AuthorizerConfig.java
@@ -0,0 +1,10 @@
+package com.ciros.authorizer;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class AuthorizerConfig {
+
+}
